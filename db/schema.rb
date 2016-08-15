17d80c00b8c761c960240ad7ff2e3f21a427d299
Anna Weisbrodt <aweisbro@gmail.com>
2016-08-15 13:56:55 -0600

    change body column data type to text

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160815192939) do
+ActiveRecord::Schema.define(version: 20160815195607) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "ideas", force: :cascade do |t|
     t.string  "title"
-    t.string  "body"
     t.integer "quality", default: 0
+    t.text    "body"
   end
 
 end
