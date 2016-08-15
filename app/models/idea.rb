853ef5196a4056ff48072a411e1fa18090fa7ca4
Anna Weisbrodt <aweisbro@gmail.com>
2016-08-15 15:31:27 -0600

    feature test for idea view passing

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -8,4 +8,8 @@ class Idea < ApplicationRecord
   def formatted_updated_at
     updated_at.localtime.strftime("%m/%d/%Y %I:%M%p")
   end
+
+  def truncated_body
+    body.truncate_words(100)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,9 @@ class Seed
   def create_ideas
     Idea.create(title: "Veggie Truck", body: "vegan food, in a truck")
     Idea.create(title: "Photo Signature", body: "app that places small photo of the photographer in the corner of the photo", quality: 1)
+    Idea.create(title: "Long Idea",
+                  body: "Pitchfork biodiesel dreamcatcher, DIY mumblecore jean shorts keytar poutine. Humblebrag fingerstache kickstarter 90's, narwhal blog squid roof party banh mi poutine tacos. Skateboard pork belly kombucha bitters master cleanse actually, pabst echo park quinoa selvage pug crucifix synth cornhole. Salvia vinyl listicle knausgaard offal, scenester echo park beard normcore authentic whatever chartreuse 8-bit VHS. Tacos put a bird on it semiotics ugh squid neutra fingerstache, kickstarter poutine truffaut mixtape flexitarian before they sold out viral. Seitan gentrify tousled, swag cardigan fingerstache mlkshk small batch leggings retro +1 kinfolk messenger bag waistcoat selvage. Salvia swag messenger bag, fixie franzen waistcoat cronut. Vinyl 90's pour-over paleo, tilde sustainable gluten-free. Mumblecore small batch yuccie bespoke, church-key blog meditation crucifix flexitarian listicle migas butcher XOXO brunch. Meditation microdosing flannel chia, pug normcore typewriter actually. Slow-carb mustache kickstarter williamsburg, lomo +1 direct trade tacos iPhone yr drinking vinegar pug neutra church-key. Migas 90's narwhal you probably haven't heard of them, gluten-free affogato distillery cliche food truck jean shorts humblebrag meggings. Thundercats intelligentsia mlkshk, gluten-free plaid iPhone brooklyn etsy keytar polaroid wolf fashion axe. Cronut pinterest chillwave, fashion axe hoodie heirloom semiotics gochujang meditation put a bird on it ramps sartorial cold-pressed bicycle rights austin.",
+                  created_at: "2016-08-01 9:10:00")
   end
 end
 
