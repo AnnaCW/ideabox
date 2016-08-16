52730f0c157c9c8f8686a9ee9151901c293743ae
Anna Weisbrodt <aweisbro@gmail.com>
2016-08-16 13:45:03 -0600

    change quality

--- app/assets/javascripts/ideas.js
+++ app/assets/javascripts/ideas.js
@@ -8,7 +8,11 @@ function displayIdea(idea) {
   return $('<tr id=' + idea.id + '>' +
   '<td>' + idea.title + '</td>' +
   '<td>' + jQuery.trim(idea.body).split(" ").slice(0, 100).join(" ") + '</td>' +
-  '<td>' + idea.quality + '</td>'  +
+  '<td class=quality>' + idea.quality + '</td>' +
+  '<td>' +
+    '<button class="btn btn-xs btn-success thumbs-up-button" data-target=' + idea.id + '>Thumbs Up</button>' +
+    '<button class="btn btn-xs btn-warning thumbs-down-button" data-target=' + idea.id + '>Thumbs Down</button>' +
+  '</td>' +
   '<td><button class="btn btn-danger delete-button" data-target=' + idea.id + '>Delete</button>' +
   '</tr>');
 };
@@ -27,7 +31,11 @@ function displayIdea(idea) {
         $(".ideas-listing").prepend('<tr id=' + idea.id + '>' +
         '<td>' + idea.title + '</td>' +
         '<td>' + jQuery.trim(idea.body).split(" ").slice(0, 100).join(" ") + '</td>' +
-        '<td>' + idea.quality + '</td>' +
+        '<td class="quality">' + idea.quality + '</td>' +
+        '<td>' +
+          '<button class="btn btn-xs btn-success thumbs-up-button" data-target=' + idea.id + '>Thumbs Up</button>' +
+          '<button class="btn btn-xs btn-warning thumbs-down-button" data-target=' + idea.id + '>Thumbs Down</button>' +
+        '</td>' +
         '<td><button class="btn btn-danger delete-button" data-target=' + idea.id + '>Delete</button>' +
         '</tr>');
         $("#titleTextarea").val("");
@@ -57,7 +65,63 @@ function displayIdea(idea) {
        console.log(xhr.responseText)
      }
     });
-
   })
+
+  var increaseQuality = {
+    swill: 'plausible',
+    plausible: 'genius',
+    genius: 'genius'
+  };
+
+  var decreaseQuality = {
+    swill: 'swill',
+    plausible: 'swill',
+    genius: 'plausible'
+  };
+
+  $('.ideas-listing').delegate('.thumbs-up-button', 'click', function() {
+    var ideaId = $(this).data("target")
+    var idea = $(this).parent().parent()
+    var ideaQuality = $(this).parent().parent().find(".quality").text();
+    var data = { quality: increaseQuality[ideaQuality] };
+    console.log(data);
+
+    $.ajax({
+      url: "/api/v1/ideas/" + ideaId + ".json",
+      method: "PATCH",
+      dataType: "JSON",
+      data: data,
+      success: function(updatedIdea){
+        console.log("increased quality");
+        $(idea).find(".quality").text(increaseQuality[ideaQuality])
+      },
+      error: function(xhr) {
+       console.log(xhr.responseText)
+     }
+    });
+  })
+
+  $('.ideas-listing').delegate('.thumbs-down-button', 'click', function() {
+    var ideaId = $(this).data("target")
+    var idea = $(this).parent().parent()
+    var ideaQuality = $(this).parent().parent().find(".quality").text();
+    var data = { quality: decreaseQuality[ideaQuality] };
+    console.log(data);
+
+    $.ajax({
+      url: "/api/v1/ideas/" + ideaId + ".json",
+      method: "PATCH",
+      dataType: "JSON",
+      data: data,
+      success: function(updatedIdea){
+        console.log("decreased quality");
+        $(idea).find(".quality").text(decreaseQuality[ideaQuality])
+      },
+      error: function(xhr) {
+       console.log(xhr.responseText)
+     }
+    });
+  })
+
 
 });
