df3cbbcb4913203d29b3bf07b6e01ef19cb700a2
Anna Weisbrodt <aweisbro@gmail.com>
2016-08-16 22:03:57 -0600

    edit body working

--- app/assets/javascripts/edit_ideas.js
+++ app/assets/javascripts/edit_ideas.js
@@ -1,24 +1,24 @@
 $(document).ready(function(){
-
    $.when(editIdeaTitle())
-  
+   $.when(editIdeaBody())
 })
 
-  function editIdeaTitle() {
-    $('.ideas-listing').on('click', '.title', function(e) {
-      $('.ideas-listing').off('click', '.title');
-        var ideaTitle = $(this).parent().find(".title").text();
-        $(this).parent().find(".title").html("<textarea class='form-control'>" + ideaTitle + "</textarea>");
-        var ideaTitleArea = $(this).parent().find(".title").find("textarea");
-        ideaTitleArea.addClass("edited");
+function editIdeaTitle() {
+  $('.ideas-listing').on('click', '.title', function(e) {
+    $('.ideas-listing').off('click', '.title');
+      var ideaTitle = $(this).parent().find(".title").text();
+      $(this).parent().find(".title").html("<textarea class='form-control'>" + ideaTitle + "</textarea>");
 
-        $(".ideas-listing").on('click', function(event) {
-          if(!$(event.target).is(".edited") ) {
+      var ideaTitleArea = $(this).parent().find(".title").find("textarea");
+      ideaTitleArea.addClass("edited");
 
-            var ideaId = $(".edited").parent().parent().attr("id");
-            var updatedTitle = $(this).parent().find(".title").find("textarea").val();
+      $(".ideas-listing").on('click', function(event) {
+        if(!$(event.target).is(".edited") ) {
 
-            var data = { title: updatedTitle };
+          var ideaId = $(".edited").parent().parent().attr("id");
+          var updatedTitle = $(this).parent().find(".title").find("textarea").val();
+
+          var data = { title: updatedTitle };
 
             $.ajax({
                 url: "/api/v1/ideas/" + ideaId + ".json",
@@ -27,60 +27,55 @@ $(document).ready(function(){
                 data: data,
                 success: function(updatedIdea){
                   console.log("updated!");
-                  $(".edited").parent().html("<td class='title'>" + updatedTitle + "</td>");
+                  // $(".edited").parent().html("<td class='title'>" + updatedTitle + "</td>");
+                  $(".edited").removeClass("edited");
+
 
                   },
                 error: function(xhr) {
                     console.log(xhr.responseText)
                   }
-              });
-
-          }
-        })
-
-        })
-    }
-
-        function updateIdeaTitle() {
-          $("document").on('click', function(event) {
-            // if(!$(event.target).is(".edited") ) {
-              console.log("win");
-            // $('.ideas-listing').on('click', '.body', function() {
-            // var ideaId = $(this).parent().find("id");
-            // var updatedBody = $(this).parent().find(".full-body").text();
-            // }
-          })
-         }
+             });
+        }
+    })
+  })
+ }
 
 
+function editIdeaBody() {
+  $('.ideas-listing').on('click', '.body', function() {
+    $('.ideas-listing').off('click', '.body');
+      var ideaBody = $(this).parent().find(".full-body").text();
+      $(this).parent().find(".body").html("<textarea class='form-control'>" + ideaBody + "</textarea>");
 
-        //     var data = { title: updatedTitle };
-        //
-        //     $.ajax({
-        //       url: "/api/v1/ideas/" + ideaId + ".json",
-        //       method: "PATCH",
-        //       dataType: "JSON",
-        //       data: data,
-        //       success: function(updatedIdea){
-        //         console.log("updated!");
-        //       },
-        //         error: function(xhr) {
-        //           console.log(xhr.responseText)
-        //       }
-        //       });
-        //     }
-        //   })
-        // }
-        //
+      var ideaBodyArea = $(this).parent().find(".body").find("textarea");
+      ideaBodyArea.addClass("edited");
 
+      $(".ideas-listing").on('click', function(event) {
+        if(!$(event.target).is(".edited") ) {
 
+          var ideaId = $(".edited").parent().parent().attr("id");
+          var updatedBody = $(this).parent().find(".body").find("textarea").val();
 
+          var data = { body: updatedBody };
 
-  function editIdeaBody() {
-    $('.ideas-listing').on('click', '.body', function() {
-      $('.ideas-listing').off('click', '.body');
-        var ideaBody = $(this).parent().find(".full-body").text();
-        $(this).parent().find(".body").html("<textarea class='form-control'>" + ideaBody + "</textarea>");
+            $.ajax({
+                url: "/api/v1/ideas/" + ideaId + ".json",
+                method: "PATCH",
+                dataType: "JSON",
+                data: data,
+                success: function(updatedIdea){
+                  console.log("updated!");
+                  // $(".edited").parent().html("<td class='body'>" + updatedBody + "</td>");
+                  $(".edited").removeClass("edited");
+
+                  },
+                error: function(xhr) {
+                    console.log(xhr.responseText)
+                  }
+             });
+        }
+    })
    })
   }
 
